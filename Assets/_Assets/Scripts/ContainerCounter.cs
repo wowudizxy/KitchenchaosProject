253a351c3fa4acef320aed4203e316da229bba2e
zhangxingyan <132345690+wowudizxy@users.noreply.github.com>
2024-05-25 21:40:32 +0800

    实现ContainerCounter的动画效果

--- Assets/_Assets/Scripts/ContainerCounter.cs
+++ Assets/_Assets/Scripts/ContainerCounter.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ContainerCounter : BaseCounter
 {
     [SerializeField]private KitchenObjectSO kitchenObjectSO;
+    [SerializeField]private ContainerCounterVisual containerCounterVisual;
     public override void Interact(Player player)
     {
         if(player.IsHaveKitchenObject()) return;
+        containerCounterVisual.OpenClose();
         CreateKitchenObject(kitchenObjectSO.kitchenObject);
         TransferKitchenObject(this,player);
     }
---
+++ Assets/_Assets/Scripts/ContainerCounterVisual.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounterVisual : MonoBehaviour
+{
+    private Animator anim;
+    private void Start() {
+        anim = GetComponent<Animator>();
+    
+    }
+    public void OpenClose(){
+        anim.SetTrigger("OpenClose");
+    }
+}
